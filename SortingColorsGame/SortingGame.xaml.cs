991cafc74e9b82b601994d769e987b392d7b2aed
Maria Pia Sanchez <mapisanchezg@gmail.com>
2018-10-24 16:48:47 -0300

    Initial commit

---
+++ SortingColorsGame/SortingGame.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SortingColorsGame
+{
+    /// <summary>
+    /// Lógica de interacción para SortingGame.xaml
+    /// </summary>
+    public partial class SortingGame : Window
+    {
+        public SortingGame()
+        {
+            InitializeComponent();
+        }
+
+		public static bool Relative { get; internal set; }
+	}
+}
---
+++ SortingColorsGame/SquareBox.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingColorsGame
+{
+    class SquareBox
+    {
+        /* Attributes */
+        private int id;
+        private int color;
+        private List<Square> box;
+
+        /* Constructor */
+        public SquareBox(int id, int color, List<Square> box)
+        {
+            this.id = id;
+            this.color = color;
+            this.box = box;
+        }
+
+        /* Getter and Setter */
+        public int Color { get => color; set => color = value; }
+        public int Id { get => id; set => id = value; }
+        internal List<Square> Box { get => box; set => box = value; }
+
+        /* Append square in a box */
+        public void AppendSquare(Square square)
+        {
+            if(square.Color != this.color)
+            {
+                Console.WriteLine("Invalid square color.");
+                return;
+            }
+            box.Add(square);
+            return;
+        }
+    }
+}
